5af46cfaa70a5e481502401b9fc393ce0cd11c5f
fs <fs1904253956@qq.com>
2019-05-15 14:16:20 +0800

    pack

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import "./index.css"
+import  "./index.scss"
+ReactDom.render(
+    <h1 className="hel">hello</h1>,
+    document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,82 @@
 const path =              require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/app.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
   },
+  mode: "development",
+  module: {
+    rules: [
+      //react(jsx)语法配置
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
+      },
+      //css文件配置
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ]
+      },
+      //sass文件配置
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     {
+      //       loader: MiniCssExtractPlugin.loader,
+      //       options: {
+      //         publicPath: '../',
+      //         hmr: process.env.NODE_ENV === 'development',
+      //       },
+      //     },
+      //     'sass-loader'
+      //   ]
+      // }
+      {
+        test: /\.scss$/,
+        use: [
+            // fallback to style-loader in development
+            process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+        ]
+    }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template:'./src/index.html'
+    }),
+    // new ExtractTextPlugin("styles.css"),
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+      chunkFilename: '[id].css',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'index.sass',
+      chunkFilename: '[id].css',
     })
-  ],
-  mode: "development",
+
+  ]
+  
 };
