af4499376612571f23b1a995aa380ef3048a07ac
fs <fs1904253956@qq.com>
2019-05-15 19:03:39 +0800

    pack

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import "./index.css"
 import  "./index.scss"
-// import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './../node_modules/font-awesome/css/font-awesome.css';
 ReactDom.render(
     <div>
